6728722f7007426b632721b21adf99c3323ab967
yessebell23 <yessebell.rios@upr.edu>
2017-05-31 19:45:35 -0600

    timer

--- CollectionViewProgram/ExerciesViewController.swift
+++ CollectionViewProgram/ExerciesViewController.swift
@@ -22,14 +22,51 @@ class ExerciesViewController: UIViewController {
     }
     
     
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var time = 0
+    var timer = Timer()
+    
+    
+    @IBAction func playButton(_ sender: Any) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ExerciesViewController.TimerAction)), userInfo: nil, repeats: true)
+    }
+    
+    
+    
     @IBAction func pauseButton(_ sender: Any) {
+        
+        timer.invalidate()
+        
         print("pause")
     }
     
     @IBAction func stopButton(_ sender: Any) {
         print("stop")
+        
+        
+        
     }
-    /*
+    
+    func TimerAction()
+    {
+        time += 1
+        
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.unitsStyle = .positional
+        
+        let formattedString = formatter.string(from: TimeInterval(time))!
+        
+      
+        label.text = formattedString
+        
+    }
+    
+        /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
